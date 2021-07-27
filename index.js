106c62417a6e450ceb8ec123527eca354419be30
Hikmet Dogrul <hikmetdogrul00@gmail.com>
2021-07-27 18:58:53 -0400

    adding list of questions

--- index.js
+++ index.js
@@ -3,7 +3,54 @@ const fs = require('fs');
 const inquirer = require('inquirer');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        name: 'title',
+        message: 'Enter a project title: '
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Provide a description for the project: '
+    },
+    {
+        type: 'input',
+        name: 'installation',
+        message: 'Provide installation instructions for the project: '
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'Provide usage information for the project: '
+    },
+    {
+        type: 'input',
+        name: 'contributing',
+        message: 'Provide contribution guidelines for the project: '
+    },
+    {
+        type: 'input',
+        name: 'tests',
+        message: 'Provide test instructions for the project: '
+    },
+    {
+        type: 'list',
+        name: 'license',
+        message: 'Choose a license for the project: ',
+        choices: ['MIT', 'Apache', 'GPL', 'GPL v3', 'LGPL v3', 'None']
+    },
+    {
+        type: 'input',
+        name: 'github',
+        message: 'Enter your GitHub username: '
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'Enter your email address: '
+    }
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
